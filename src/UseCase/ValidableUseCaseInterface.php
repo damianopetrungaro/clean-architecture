f480489a1f033f5923e97adbe716d423a015a139
Damiano Petrungaro <damianopetrungaro@gmail.com>
2017-01-21 02:55:31 +0100

    Added ValidableUseCaseInterface

--- src/UseCase/ValidableUseCaseInterface.php
+++ src/UseCase/ValidableUseCaseInterface.php
@@ -6,14 +6,16 @@ namespace Damianopetrungaro\CleanArchitecture\UseCase;
 use Damianopetrungaro\CleanArchitecture\UseCase\Request\RequestInterface;
 use Damianopetrungaro\CleanArchitecture\UseCase\Response\ResponseInterface;
 
-interface UseCaseInterface
+interface ValidableUseCaseInterface extends UseCaseInterface
 {
     /**
-     * Method to call for initialize the use case.
-     * You must use a reference to ResponseInterface to return the response.
+     * Method to call for validate an UseCase.
+     * You must use a reference to ResponseInterface to add errors to response.
      *
      * @param RequestInterface $request
      * @param ResponseInterface $response
+     *
+     * @return bool
      */
-    public function __invoke(RequestInterface $request, ResponseInterface $response) : void;
+    public function isValid(RequestInterface $request, ResponseInterface $response) : bool;
 }
