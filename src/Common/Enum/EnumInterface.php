0e65ebc7aa8176a92ec1297e10d442e14974e3e9
Damian <bigfootdd@gmail.com>
2017-04-11 18:43:05 +0200

    Merge from Upstream

--- src/Common/Enum/EnumInterface.php
+++ src/Common/Enum/EnumInterface.php
@@ -32,4 +32,11 @@ interface EnumInterface
      * @return string
      */
     public function getValue(): string;
+
+    /**
+     * Return the enum value
+     *
+     * @return string
+     */
+    public function __toString(): string;
 }
