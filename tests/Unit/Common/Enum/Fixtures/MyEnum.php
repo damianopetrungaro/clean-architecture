ac51701cf7c642a0651305ad7e7f822211ca2def
Damiano Petrungaro <damianopetrungaro@gmail.com>
2017-04-15 15:15:23 +0200

    Added comments on Fixtrures/MyEnum for IDE supports

--- tests/Unit/Common/Enum/Fixtures/MyEnum.php
+++ tests/Unit/Common/Enum/Fixtures/MyEnum.php
@@ -4,6 +4,12 @@ namespace Damianopetrungaro\CleanArchitecture\Unit\Common\Enum\Fixtures;
 
 use Damianopetrungaro\CleanArchitecture\Common\Enum\Enum;
 
+/**
+ * @method static MyEnum A()
+ * @method static MyEnum B()
+ * @method static MyEnum C()
+ * @method static MyEnum VALUE_NUMB()
+ */
 final class MyEnum extends Enum
 {
     const A = 'VALUE_A';
